8d1ca0fce26326793eea8240b49da902bb929331
tahy2201 <hyg121arx8@gmail.com>
2025-11-13 10:27:03 +0900

    Vite + React追加

---
+++ client/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Gmail Filter Manager</h1>
+      <p>Phase 1 開発中...</p>
+    </div>
+  );
+}
+
+export default App;
